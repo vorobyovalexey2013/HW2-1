f175f2c8e03ba831272ca9ce692140a6e6e1060f
Alexey <valeskey1988@mail.ru>
2023-02-15 20:08:51 +0500

    first comm

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 13676;
+        int chek = 20;
+        int bonus = price / chek;
+        System.out.println("Количество начисленных бонусов" + -bonus);
+    }
+}
